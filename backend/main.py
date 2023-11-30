4b9177043a2af37b9b613a94df5a94a281736f9c
おれ <jiiiiiiiiiiiiiiin@gmail.com>
2023-11-30 15:21:26 +0900

    oasuhi

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from backend import app
 from flask import render_template
-#import pandas as pd
+import pandas as pd
+import os
 
 @app.route('/')
 def index():
@@ -13,3 +14,12 @@ def form():
     return render_template(
         'form.html'
     )
+
+@app.route('/ranking')
+def ranking():
+    return render_template(
+        'ranking.html'
+    )
+def count(): #CSVファイルのうち必要なものだけをピックアップして行き先の数をカウントする。
+    csv_file = os.listdir('/Users/ryoyajindo/daijikken/Osushi/backend/data_result')
+    
